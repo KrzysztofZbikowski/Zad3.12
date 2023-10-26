bdac73f41a6cd3d85b33200a5ccf54caecba019a
Zbiku <Zbiku@MSI>
2023-10-26 22:49:54 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int suma = 0, liczba;
+        do
+        {
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+            liczba = int.Parse(Console.ReadLine());
+            suma = suma + liczba;
+        } while (liczba != 0);
+        Console.WriteLine("Suma wprowadzonych liczb wynosi " + suma);
+        Console.ReadKey();
+    }
+}
